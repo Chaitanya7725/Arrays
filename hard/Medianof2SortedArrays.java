88862c92975b53eb51970a7477cdd21352a91653
Chaitanya7725 <chaitanya7725@gmail.com>
2024-02-14 13:38:46 -0600

    Time complexity if O(n log(n)) which is more than required

--- hard/Medianof2SortedArrays.java
+++ hard/Medianof2SortedArrays.java
@@ -5,53 +5,44 @@ import java.util.Arrays;
 public class Medianof2SortedArrays {
 
     public static void main(String[] args) {
-//        findMedianSortedArrays(new int[]{}, new int[] {});
-//        findMedianSortedArrays(new int[]{}, new int[] {1,2,3});
-//        findMedianSortedArrays(new int[]{1}, new int[] {1,2,3});
-//        findMedianSortedArrays(new int[]{1,2,3}, new int[] {1,2,3});
-        findMedianSortedArrays(new int[]{3,4,5}, new int[] {1,2,3});
+        System.out.println(findMedianSortedArrays(new int[]{1,3},new int[]{2})); // Output: 2.0
+        System.out.println(findMedianSortedArrays(new int[]{1,3,9},new int[]{2,5,8})); // Output: 4.0
+        System.out.println(findMedianSortedArrays(new int[]{1,2},new int[]{3,4})); // Output: 2.5
     }
 
-    public static double findMedianSortedArrays(int[] n1, int[] n2) {
-        if(n1.length == 0 && n2.length == 0) return 0;
-        if(n1.length != 0 && n2.length == 0)
-            return n1.length % 2 == 0 ? (double)(n1[n1.length/2]+n1[(n1.length/2)-1])/2 : n1[n1.length]/2-1 ;
-        if(n1.length == 0 && n2.length != 0)
-            return n2.length % 2 == 0 ? (double)(n2[n2.length/2]+n2[(n2.length/2)-1])/2 : n2[n2.length]/2-1 ;
-        int [] m = new int[n1.length + n2.length];
-        int i = 0;
-        int n1_index = 0;
-        int n2_index = 0;
-        int length = Integer.max(n1.length, n2.length);
-        while(length-- > 0){
-            int n1_val = n1_index < n1.length ? n1[n1_index] : -1;
-            int n2_val = n2_index < n2.length ? n2[n2_index] : -1;
-            if(n1_val <= n2_val){
-                if(n1_val > 0){
-                    m[i] = n1_val;
-                    i++;
-                    n1_index++;
-                }
-                if(n2_val > 0){
-                    m[i] = n2_val;
-                    i++;
-                    n2_index++;
-                }
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int left = nums1.length-1;
+        int right = 0;
+        while(left >= 0 && right < nums2.length){
+            if(nums1[left] > nums2[right] ){
+                int temp = nums1[left];
+                nums1[left] = nums2[right];
+                nums2[right] = temp;
+                left --;
+                right ++;
             }else{
-                if(n2_val > 0){
-                    m[i] = n2_val;
-                    i++;
-                    n2_index++;
-                }
-                if(n1_val > 0){
-                    m[i] = n1_val;
-                    i++;
-                    n1_index++;
-                }
+                break;
             }
         }
-        System.out.println(Arrays.toString(m));
-        return 0;//m.length % 2 == 0 ? (double)(m[m.length/2]+m[(m.length/2)-1])/2 : m[m.length]/2-1 ;
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int mid = nums1.length + nums2.length;
+        if(mid % 2 ==0){
+            int first = ((mid/2)-1);
+            int second = mid/2;
+            int sum = 0;
+            if(first<nums1.length)
+                sum+=nums1[first];
+            if(second<nums2.length)
+                sum+=nums2[second-first-1];
+            if(first>nums1.length-1)
+                sum+=nums1[first];
+            if(second>nums2.length-1)
+                sum+=nums2[second-first-1];
+            return sum/2.0;
+        }else{
+             return mid/2 < nums1.length ? nums1[mid/2] :nums2[mid/2];
+        }
     }
 
 }
