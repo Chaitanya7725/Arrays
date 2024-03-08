9aaf272b03508d5336a5e9ef3491de64229e21d8
Chaitanya7725 <chaitanya7725@gmail.com>
2024-03-08 13:19:16 -0600

    two pointer approach considering the current element and ++/-- repetitive element

--- medium/ThreeSum15.java
+++ medium/ThreeSum15.java
@@ -7,31 +7,33 @@ import java.util.List;
 public class ThreeSum15 {
 
     public static void main(String[] args) {
-        System.out.println(threeSum(new int[]{-1,0,1,2,-1,-4}));
+        System.out.println(threeSum(new int[]{1,-1,-1,0}));
     }
-
+//    [1,-1,-1,0]
     public static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
-        for (int i =0;i<nums.length-1;i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                for (int k = j+1; k < nums.length; k++) {
-                    int a=nums[i];
-                    int b=nums[j];
-                    int c=nums[k];
-                    if((a+b+c)==0 && !result.contains(new ArrayList<>(){{
-                        add(a);
-                        add(b);
-                        add(c);
-                    }}))
-                        result.add(new ArrayList<>(){{
-                            add(a);
-                            add(b);
-                            add(c);
-                        }});
+        for (int i = 0; i < nums.length-2; i++) {
+            if(i==0 || i> 0 && nums[i] !=nums[i-1] ){
+                int a = i+1;
+                int b = nums.length-1;
+                int sum = 0 - nums[i];
+                while(a < b){
+                    if(nums[a] + nums[b] == sum ){
+                        result.add(Arrays.asList(nums[i],nums[a],nums[b]));
+                        while(a < b && nums[a]==nums[a+1]) a++;
+                        while(a < b && nums[b]==nums[b-1]) b--;
+                        a++;
+                        b--;
+                    }   else if(nums[a]+ nums[b] > sum){
+                        b--;
+                    } else{
+                        a++;
+                    }
                 }
             }
         }
+
         return result;
     }
 
