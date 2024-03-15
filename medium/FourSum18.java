f3536c879ac61cc21f72cc3829560ff80aee646e
Chaitanya7725 <chaitanya7725@gmail.com>
2024-03-14 23:43:42 -0500

    Not working Recursive approach

--- medium/FourSum18.java
+++ medium/FourSum18.java
@@ -6,48 +6,49 @@ import java.util.List;
 
 public class FourSum18 {
 
+    static List<Integer> quad;
+    static List<List<Integer>> result  = new ArrayList<>();
     public static void main(String[] args) {
-        int[] nums = {2,2,2,2,2};
-        System.out.println(fourSum(nums, 8));
+        int[] nums = {1,0,-1,0,-2,2};
+        System.out.println(fourSum(nums, 0));
     }
 
     public static List<List<Integer>> fourSum(int[] nums, int target) {
+        quad = new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> result  = new ArrayList<>();
-        if(nums.length< 4) return result;
-        for (int i = 0; i < nums.length-3; i++) {
-            for (int j = i+1; j < nums.length-2; j++) {
-                int k = j+1;
-                int l = nums.length-1;
-
-                while(k<l){
-                    int sum = nums[i]+nums[j]+nums[k]+nums[l];
-                    if(sum == target){
-                        int finalI = i;
-                        int finalJ = j;
-                        int finalK = k;
-                        int finalL = l;
-                        result.add(new ArrayList<>(){{
-                            add(nums[finalI]);
-                            add(nums[finalJ]);
-                            add(nums[finalK]);
-                            add(nums[finalL]);
-                        }});
-                        while(nums[k]==nums[k+1]) k++;
-                        while(nums[l]==nums[l-1]) l--;
-                        k++;
-                        l--;
-                    } else if(sum < target){
-                        k++;
-                    } else {
-                        l--;
-                    }
-                    while(nums[j]==nums[j+1]) j++;
-                }
-                while(nums[i]==nums[i+1]) i++;
-            }
-        }
+        result  = new ArrayList<>();
+        kSum(4,0,target,nums);
         return result;
     }
+
+    public static void kSum(int k, int start, int target,int[] nums){
+        if(k!=2){
+            for (int i = start; i < nums.length-k+1; i++) {
+                if(i > start && nums[i]==nums[i-1] ){
+                    continue;
+                }
+                quad.add(nums[i]);
+                kSum(k-1,start+1,target-nums[i],nums);
+                quad.remove(0);
+            }
+            return;
+        }
+        int l=start;
+        int r=nums.length-1;
+        while(l<r){
+            if(nums[l]+nums[r]< target)
+                l++;
+            else if (nums[l]+nums[r]> target)
+                r--;
+            else{
+                quad.add(nums[l]);
+                quad.add(nums[r]);
+                result.add(quad);
+                l+=1;
+                while(l<r && nums[l]== nums[l-1]) l++;
+            }
+        }
+    }
+
 
 }
